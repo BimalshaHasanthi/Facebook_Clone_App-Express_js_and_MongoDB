27445c11672368315073d95df123e74a9d441cbb
BimalshaHasanthi <bimalshahasanthi@gmail.com>
2022-09-07 20:39:51 +0530

    Updated content

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const port = 4500
+
+const url = 'mongodb://localhost/facebook'
+const user = require('../facebookclone-master/routes/users.js')
+const post = require('../facebookclone-master/routes/posts.js')
+
+mongoose.connect(url,{ useNewUrlParser: true })
+const con = mongoose.connection
+
+con.on("open", () => {
+    console.log('MongoDB Connected!')
+})
+
+app.use(express.json())
+
+app.use('/user', user)
+app.use('/post', post)
+
+app.listen(port, () => {
+    console.log(`App is listening on port -  ${port}`);
+})
